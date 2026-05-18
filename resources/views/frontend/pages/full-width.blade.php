ecb6a977bd7f9aaba6dcb9e9d5cc8ef5fe3818a5
AjeetDots <ajeet.singh@dotsquares.com>
2026-05-18 10:30:42 +0530

    update defaul page

--- resources/views/frontend/pages/full-width.blade.php
+++ resources/views/frontend/pages/full-width.blade.php
@@ -11,7 +11,7 @@
         @include('frontend.pages.partials.cms-hero', ['page' => $page])
 
         <div class="cms-builder">
-            <div class="container-fluid cms-page-container cms-builder__shell px-3 px-sm-4 px-xl-5">
+            <div class="container cms-page-container cms-builder__shell">
                 @include('frontend.pages.partials.cms-body-inner', ['page' => $page, 'layout' => 'full'])
             </div>
         </div>
